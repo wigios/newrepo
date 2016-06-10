33a4a36fa7d47ec05856a5681c3fe079dfc1d5d5
wospina <wigios@gmail.com>
2016-06-10 14:55:25 -0500

    aupdate by data excel

---
+++ SeleniumUnit/src/test/java/Utility/Constant.java
@@ -0,0 +1,15 @@
+package Utility;
+ 
+   public class Constant {
+ 
+      public static final String URL = "http://www.store.demoqa.com";
+ 
+      public static final String Username = "testuser_1";
+ 
+      public static final String Password = "Test@123";
+ 
+      public static final String Path_TestData = "C://Users//wospina//git//newrepo//SeleniumUnit//src//test//java//testData//";
+ 
+      public static final String File_TestData = "TestData.xlsx";
+ 
+   }
